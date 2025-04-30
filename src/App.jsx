875ebd482740bfdfd560cbf2825e165eed3daa83
unknown <venuslove1704@gmail.com>
2025-04-30 13:48:24 -0700

    se han realizado cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react"
+import './app.css'
+
+import animalshow from './animalshow'
+
+function getRandomAnimal(){
+  const animals = ['cat', 'bird', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random()* animals.length)]
+}
+
 function App() {
 
+  const [animals, setAnimals] = useState([])
+  
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <animalshow type={animal} key={index}/>
+  })
+
   return (
-    <>
-     <h1>Animals</h1> 
-    </>
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div  className="animal-list">
+        {renderedAnimals}
+      </div> 
+    </div>
   )
 }
 
